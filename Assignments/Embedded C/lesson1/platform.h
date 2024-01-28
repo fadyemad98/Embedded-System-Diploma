a3ee2a8d09cd221da03cae02b36c0c8d25c8fbe5
fady Emad Shoukry <121491487+fadyemad98@users.noreply.github.com>
2024-01-28 19:53:01 +0200

    Add files via upload

---
+++ Assignments/Embedded C/lesson1/platform.h
@@ -0,0 +1,30 @@
+/*
+ * platform.h
+ *
+ *  Created on: Dec 13, 2020
+ *      Author: HP
+ */
+
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+#include <stdio.h>
+typedef unsigned char boolean;
+typedef signed char sint8;
+typedef unsigned char uint8;
+typedef signed short sint16;
+typedef unsigned short uint16;
+typedef signed long sint32;
+typedef signed long long sint64;
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+typedef unsigned short uint8_least;
+typedef unsigned short uint16_least;
+typedef unsigned long uint32_least;
+typedef signed short sint8_least;
+typedef signed short sint16_least;
+typedef signed long sint32_least;
+typedef float float32;
+typedef double float64;
+
+
+#endif /* PLATFORM_H_ */
